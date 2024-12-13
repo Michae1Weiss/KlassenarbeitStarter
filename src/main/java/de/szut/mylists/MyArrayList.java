6bd0e2228a17737e37e17c115b22ca868ce4474c
mykhailo.bilyi <johny.johny.davis@gmail.com>
2024-12-13 10:09:15 +0100

    Created signatures of a MyArrayList

--- src/main/java/de/szut/mylists/MyArrayList.java
+++ src/main/java/de/szut/mylists/MyArrayList.java
@@ -1,5 +1,47 @@
 package de.szut.mylists;
 
 public class MyArrayList {
+    /** Gibt die Anzahl der gespeicherten Zahlen zurück.
+     *
+     * @return
+     */
+    int size() {
 
+    }
+
+    /** Fügt eine Zahl am Ende der Liste ein.
+     *
+     * @return
+     */
+    void add (int value) {
+
+    }
+
+    /** Gibt den Wert an der Stelle index zurück.
+     *
+     * @return
+     */
+    int get(int index) {
+
+    }
+
+    /** Entfernt die Zahl an der mit index angegeben Stelle.
+     * Gibt es den Index nicht, wirft die Methode eine RunTimeException
+     * mit der Fehlermeldung „Dieser Index existiert nicht!“.
+     * Alle Zahlenwerte rechts vom gelöschten Wert
+     * rücken im Array eine Position nach links.
+     *
+     * @return
+     */
+    void remove(int index) {
+
+    }
+
+    /** Überprüft, ob die Zahl value in der Liste vorhanden ist.
+     *
+     * @return
+     */
+    boolean contains(int value) {
+
+    }
 }
