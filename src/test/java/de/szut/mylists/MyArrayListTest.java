cb1a886e82858ded6635ea2bc1e42a906f7c8af9
mykhailo.bilyi <johny.johny.davis@gmail.com>
2024-12-13 10:15:37 +0100

    Created signatures for a tests

--- src/test/java/de/szut/mylists/MyArrayListTest.java
+++ src/test/java/de/szut/mylists/MyArrayListTest.java
@@ -1,5 +1,49 @@
 package de.szut.mylists;
 
-public class MyArrayListTest {
+import org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class MyArrayListTest {
+    private MyArrayList arrayList;
+
+    @BeforeEach
+    public void setup() {
+        arrayList = new MyArrayList();
+    }
+
+    /* Testing size method */
+
+    @Test
+    public void givenEmptyArrayList_whenSize_thenZero() {
+
+    }
+
+    /* Testing add method */
+
+    @Test
+    public void givenEmptyArrayList_whenAdd1_thenSize1() {
+
+    }
+
+    /* Testing get method */
+
+    @Test
+    public void givenEmptyArrayList_whenGetIndex0_thenException() {
+
+    }
+
+    /* Testing remove method */
+
+    @Test
+    public void givenEmptyArrayList_whenRemoveIndex0_thenException() {
+
+    }
+
+    /* Testing contains method */
+
+    @Test
+    public void givenEmptyArrayList_whenContains0_thenFalse() {
+
+    }
 }
